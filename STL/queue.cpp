d90b168d3d908a6fd4db69984638e6254af1af78
chintan <patelchintan843@gmail.com>
2023-09-14 20:01:48 +0530

    queue

---
+++ STL/queue.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    queue<int> q;
+
+    q.push(1);
+    q.push(2);
+    cout << q.back() << endl;
+
+    q.pop();
+    return 0;
+}
