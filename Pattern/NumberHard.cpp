6a80792232b200a373f1d6a93e11e0aa5c77ce7e
chintan <patelchintan843@gmail.com>
2023-09-14 17:29:11 +0530

    second approach

--- Pattern/NumberHard.cpp
+++ Pattern/NumberHard.cpp
@@ -20,8 +20,27 @@ void print(int n)
     }
 }
 
+void print1(int n)
+{
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+
+        for (int j = 0; j < 2 * n - 1; j++)
+        {
+            int top = i;
+            int left = j;
+            int right = (2 * n - 2) - j;
+            int down = (2 * n - 2) - i;
+            // take minimum distance
+            cout << (n - min(min(top, down), min(left, right)));
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     print(3);
+    print1(3);
     return 0;
 }
