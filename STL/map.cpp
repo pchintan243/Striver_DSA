506c79b94a8246922676521d3d89f76cf666af44
chintan <patelchintan843@gmail.com>
2023-09-15 15:10:21 +0530

    map

---
+++ STL/map.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <map>
+using namespace std;
+
+int main()
+{
+    map<int, int> m;
+    // map stores data in key's sorted order.
+
+    m.insert({2, 3});
+    m.insert(pair<int, int>(5, 50));
+    m.insert(pair<int, int>(4, 20));
+
+    m[2] = 10;
+    // key should be unique.
+
+    for (auto it : m)
+    {
+        cout << it.first << endl;
+        cout << it.second << endl;
+    }
+
+    return 0;
+}
