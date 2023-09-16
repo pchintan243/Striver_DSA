a5d41f02bdd19d0a2b5829a068a84208c2842883
chintan <patelchintan843@gmail.com>
2023-09-16 20:30:52 +0530

    right rotation

--- Vector/RotateElement.cpp
+++ Vector/RotateElement.cpp
@@ -5,19 +5,41 @@ int main()
 {
 
     vector<int> v;
-    int d = 3;
+    int d = 2;
     for (int i = 0; i < 6; i++)
     {
         v.push_back(i + 1);
     }
     d = d % v.size();
-
-    reverse(v.begin(), v.begin()+d);
-    reverse(v.begin()+d, v.end());
+    // for left rotation
+    reverse(v.begin(), v.begin() + d);
+    reverse(v.begin() + d, v.end());
     reverse(v.begin(), v.end());
+    cout << "Left rotation:" << endl;
     for (int i = 0; i < 6; i++)
     {
         cout << v[i] << " ";
+    }
+
+    cout << endl;
+
+    vector<int> v1;
+    int d1 = 2;
+    for (int i = 0; i < 6; i++)
+    {
+        v1.push_back(i + 1);
+    }
+    d1 = d1 % v1.size();
+
+    // for right rotation
+    reverse(v1.end() - d1, v1.end());
+    reverse(v1.begin(), v1.end() - d1);
+    reverse(v1.begin(), v1.end());
+
+    cout << "Right rotation:" << endl;
+    for (int i = 0; i < 6; i++)
+    {
+        cout << v1[i] << " ";
     }
     return 0;
 }
