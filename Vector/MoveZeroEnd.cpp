0a330667613365add1b10e2137393328f0ea7c64
chintan <patelchintan843@gmail.com>
2023-09-17 14:40:32 +0530

    moves zero to end

---
+++ Vector/MoveZeroEnd.cpp
@@ -0,0 +1,18 @@
+vector<int> moveZeros(int n, vector<int> a)
+{
+    int j = 0, i = 0;
+    while (i != n)
+    {
+        if (a[i] == 0)
+        {
+            i++;
+        }
+        else
+        {
+            swap(a[i], a[j]);
+            i++;
+            j++;
+        }
+    }
+    return a;
+}
