02172e8ea7da5841ec11613e4ca34e392ad7c531
chintan <patelchintan843@gmail.com>
2023-09-17 18:05:45 +0530

    first approach single occurence of number

---
+++ Vector/FindExucuteOnlyOnesNumber.cpp
@@ -0,0 +1,12 @@
+#include<vector>
+
+int getSingleElement(vector<int> &arr){
+	// Write your code here
+	int n = arr.size();
+	for(int i = 0; i < n; i=i+2) {
+		if(arr[i] != arr[i+1]) {
+			return arr[i];
+		}
+	}
+	return arr[n-1];
+}
