e448ee1c39b08122b49a32a19a40ce6bdbfa021c
chintan <patelchintan843@gmail.com>
2023-09-18 16:48:26 +0530

    sorting array of 0 1 & 2 element

---
+++ Vector/SortOf_0_1_2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+void sortArray(vector<int>& arr, int n)
+{
+    // Write your code here
+    int low = 0;
+    int mid = 0;
+    int high = n - 1;
+    while(mid <= high) {
+        if(arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(arr[high], arr[mid]);
+            high--;
+        }
+    }
+}
