3362b09dcac8a727528179feb95e751b2a0fc15a
chintan <patelchintan843@gmail.com>
2023-09-19 18:12:13 +0530

    n c r

---
+++ Maths/NcR.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n = 13, r = 3;
+    int ans = 1;
+    for (int i = 0; i < r; i++)
+    {
+        ans *= (n - i);
+        ans /= (i + 1);
+    }
+    cout << ans;
+    return 0;
+}
