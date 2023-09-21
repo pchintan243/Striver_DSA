dcb28d872c23fc63bcdd61cf6f7afc9675871ec3
chintan <patelchintan843@gmail.com>
2023-09-21 16:03:10 +0530

    lower bound

---
+++ BInary/Lower_bound.cpp
@@ -0,0 +1,22 @@
+int lowerBound(vector<int> arr, int n, int x)
+{
+    // Write your code here
+    int low = 0;
+    int high = n - 1;
+    int ans = n;
+    // Lower bound return the index of element if found otherwise it return the immediate higher element index.
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] >= x)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
