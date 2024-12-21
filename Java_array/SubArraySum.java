a9813a7f862bcd5614746dfae689a8199e52dd82
Chintan Patel <patelchintan843@gmail.com>
2024-12-21 20:19:43 +0530

    sub Array sum (negative included)

---
+++ Java_array/SubArraySum.java
@@ -0,0 +1,24 @@
+class SubArraySum {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int preSum = 0;
+        int cnt = 0;
+
+        Map<Integer, Integer> mpp = new HashMap<>();
+
+        mpp.put(0, 1);
+
+        for(int i = 0; i < n; i++) {
+
+            preSum += nums[i];
+
+            int removal = preSum - k;
+
+            cnt += mpp.getOrDefault(removal, 0);
+
+            mpp.put(preSum, mpp.getOrDefault(preSum, 0) + 1);
+        }
+
+        return cnt;
+    }
+}
