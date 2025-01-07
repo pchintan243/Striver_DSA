c25ca11802b1b05beeaaf2ffcc38085efc643f20
Chintan Patel <patelchintan843@gmail.com>
2025-01-07 21:56:39 +0530

    FindMinRotatedSorted

---
+++ Binary_Search/FindMinRotatedSorted.java
@@ -0,0 +1,26 @@
+class FindMinRotatedSorted {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = Integer.MAX_VALUE;
+
+        while(low <= high) {
+            int mid = (low + high) / 2;
+
+            // if array is sorted 
+            if(nums[low] <= nums[high]) {
+                ans = Math.min(ans, nums[low]);
+                break;
+            }
+            if(nums[low] <= nums[mid]) {
+                ans = Math.min(ans, nums[low]);
+                low = mid + 1;                
+            } else {
+                ans = Math.min(ans, nums[mid]);
+                high = mid - 1;                
+            }
+        }
+
+        return ans;
+    }
+}
