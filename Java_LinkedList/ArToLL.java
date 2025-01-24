08a3c1a2ad0832f3ac02c4e56aa8041c024980b7
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-01-24 20:48:39 +0530

    ArToLL

---
+++ Java_LinkedList/ArToLL.java
@@ -0,0 +1,16 @@
+class Solution {
+    static Node constructLL(int arr[]) {
+        // code here
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+        Node head = new Node(arr[0]);
+        Node current = head;
+        for(int i = 1; i < arr.length; i++) {
+            current.next = new Node(arr[i]);
+            current = current.next;
+        }
+        
+        return head;
+    }
+}
