6354b4b6f73f6239c55b8a4e51df33cc651fb1ee
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-01-25 23:37:46 +0530

    2D_DelAtPos

---
+++ Java_LinkedList/2D_DelAtPos.java
@@ -0,0 +1,28 @@
+class Solution {
+    public Node deleteNode(Node head, int x) {
+        // code here
+        Node current = head;
+        
+        if(x == 1) {
+            Node ans = current.next;
+            current.next = null;
+            ans.prev = null;
+            return ans;
+        }
+        
+        while(x > 1) {
+            current = current.next;
+            x--;
+        }
+        
+        if(current.next == null) {
+            current.prev.next = null;
+            current.prev = null;
+        } else {
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+        }
+        return head;
+        
+    }
+}
