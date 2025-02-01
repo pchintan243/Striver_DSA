f5d3890066543c800a275b6386f0b1ce3cac4574
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-02-01 22:04:45 +0530

    FindCycleFirstNode

---
+++ Java_LinkedList/Medium_LL/FindCycleFirstNode.java
@@ -0,0 +1,39 @@
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
+public /**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class FindCycleFirstNode {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) {
+                slow = head;
+                while(slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+} {
+    
+}
