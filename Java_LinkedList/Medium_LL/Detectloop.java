8e252c6d20a7e22e56e6948b93dc30fb4b66a198
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-02-01 20:27:42 +0530

    Detectloop

---
+++ Java_LinkedList/Medium_LL/Detectloop.java
@@ -0,0 +1,34 @@
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
+public /**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Detectloop {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null || fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+} {
+    
+}
