9a4baa878614ea96374fe932a10fdbc99b73739c
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-02-11 11:15:46 +0530

    11_02_25_1910RemoveAllOccurance

---
+++ Daily_Problem/11_02_25_1910RemoveAllOccurance.java
@@ -0,0 +1,10 @@
+class 11_02_25_1910RemoveAllOccurance {
+    public String removeOccurrences(String s, String part) {
+        StringBuilder sb = new StringBuilder(s);
+        while(sb.indexOf(part) != -1) {
+            int idx = sb.indexOf(part);
+            sb.delete(idx, idx + part.length());
+        }
+        return sb.toString();
+    }
+}
