4174fd5235a078e2df4ffef41954f7dc0103fa8d
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-02-26 19:47:34 +0530

    SubsetSum-II

---
+++ Java_Recursion/SubsetSum-II.java
@@ -0,0 +1,21 @@
+class SubsetSum-II {
+    public void generateSum(int[] nums, List<List<Integer>> ans, List<Integer> temp, int ind) {
+            ans.add(new ArrayList<>(temp));
+        
+        for(int j = ind; j < nums.length; j++) {
+            if(j > ind && nums[j] == nums[j - 1])
+                continue;
+            temp.add(nums[j]);
+            generateSum(nums, ans, temp, j + 1);
+            temp.remove(temp.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        generateSum(nums, ans, new ArrayList<>(), 0);
+
+        return ans;
+    }
+}
