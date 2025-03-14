85c98c461375ca7a69b746cba01cf2c2de89a183
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-03-14 19:32:00 +0530

    MinBitConvertNumber

---
+++ Bit Manipulation/MinBitConvertNumber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int num = start ^ goal;
+        int cnt = 0;
+        while(num != 0) {
+            cnt++;
+            num = num & (num - 1);
+        }
+
+        return cnt;
+    }
+}
