32263a64f184ff3bc68ba6a7232461477fd07ceb
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-03-14 15:56:53 +0530

    Get_Set_Clear_i-bit

---
+++ Bit Manipulation/Get_Set_Clear_i-bit.java
@@ -0,0 +1,36 @@
+class Get_Set_Clear_i-bit {
+    static void bitManipulation(int num, int i) {
+        // code here
+        StringBuilder res = new StringBuilder();
+        int storeNum = num;
+        while(storeNum != 1) {
+            if(storeNum % 2 == 1) {
+                res.append('1');
+            } else {
+                res.append('0');
+            }
+            storeNum /= 2;
+        }
+        res.append('1');
+        
+        int n = res.length();
+        if(i > n) {
+            System.out.print("0 ");
+            System.out.print((long) (num + Math.pow(2, i - 1)) + " ");
+            System.out.print(num);
+            return;
+        }
+        char ind = res.charAt(i - 1);
+        System.out.print(ind + " ");
+        if(ind == '1') {
+            System.out.print(num + " ");
+            System.out.print((long) (num - Math.pow(2, i - 1)));
+        }
+        else {
+            System.out.print((long) (num + Math.pow(2, i - 1)) + " ");
+            System.out.print(num);
+        }
+            
+        
+    }
+}
