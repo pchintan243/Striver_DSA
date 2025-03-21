a142e6a5be74b7cf32b30250de7840215f64af10
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-03-21 21:23:01 +0530

    Optimized

--- Bit Manipulation/Divide2Integer.java
+++ Bit Manipulation/Divide2Integer.java
@@ -1,28 +1,39 @@
 class Divide2Integer {
     public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        if (dividend == Integer.MIN_VALUE && divisor == 1) {
+            return Integer.MIN_VALUE;
+        }
         if(dividend == divisor)
             return 1;
         boolean flag = true;
         if((dividend >= 0 && divisor < 0) || (dividend <= 0 && divisor > 0)) {
             flag = false;
         }
-        long num = Math.abs(dividend);
-        long d = Math.abs(divisor);
+        long num = Math.abs((long) dividend);
+        long d = Math.abs((long) divisor);
         long ans = 0;
         while(d <= num) {
             int x = 0;
             while((d << (x + 1)) <= num) {
                 x++;
             }
-            num -= (d * (1 << x));
+            num -= (d << x);
             ans += (1 << x);
         }
-        if((ans == (1 << 31)) && flag) {
+
+        if(!flag) {
+            ans = -ans;
+        }
+        if(ans > Integer.MAX_VALUE) {
             return Integer.MAX_VALUE;
         }
-        if((ans == (1 << 31)) && !flag) {
+        if(ans < Integer.MIN_VALUE) {
             return Integer.MIN_VALUE;
         }
-        return flag ? (int) ans : (int) -ans;
+        
+        return (int) ans;
     }
 }
