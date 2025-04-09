5757f8e221a03e339c27c30b69270d0fe51113f4
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-04-09 21:23:43 +0530

    09_04_2025_3375_MinToMakeK

---
+++ Daily_Problem/09_04_2025_3375_MinToMakeK.java
@@ -0,0 +1,20 @@
+class 09_04_2025_3375_MinToMakeK {
+    p 
+
+        if(nums[0] < k) {
+            return -1;
+        }
+        int n = nums.length;
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] > k) {
+                cnt++;
+                while(i + 1 < n && nums[i] == nums[i + 1]) {
+                    i++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+}
