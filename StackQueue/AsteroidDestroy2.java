bf20b5a56970ade53bf3457e6574aafaaf2feed0
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-04-12 19:35:00 +0530

    AsteroidDestroy Optimized

--- StackQueue/AsteroidDestroy2.java
+++ StackQueue/AsteroidDestroy2.java
@@ -1,23 +1,21 @@
 import java.util.Stack;
-class AsteroidDestroy {
+class AsteroidDestroy2 {
     public int[] asteroidCollision(int[] asteroids) {
         int n = asteroids.length;
         Stack<Integer> st = new Stack<>();
         for(int i = 0; i < n; i++) {
-            while((!st.isEmpty() && st.peek() > 0) && (asteroids[i] < 0)) {
-                if(st.peek() == -asteroids[i]) {
+            if(asteroids[i] > 0) {
+                st.push(asteroids[i]);
+            } else {
+                while((!st.isEmpty() && st.peek() > 0) && (asteroids[i] < 0) && (st.peek() < -asteroids[i])) {
                     st.pop();
-                    asteroids[i] = 0;
-                    break;
-                } else if(st.peek() < -asteroids[i]) {
+                }
+                if(!st.isEmpty() && st.peek() == -asteroids[i]) {
                     st.pop();
-                } else {
-                    asteroids[i] = 0;
-                    break;
+                } else if(st.isEmpty() || st.peek() < 0) {
+                    st.push(asteroids[i]);
                 }
             }
-            if(asteroids[i] != 0)
-                st.push(asteroids[i]);
         }
         int[] ans = new int[st.size()];
         for(int i = st.size() - 1; i >= 0; i--) {
