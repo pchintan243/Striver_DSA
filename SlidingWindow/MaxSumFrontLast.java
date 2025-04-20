5a0b487759d68bf2f105f4971bd42e684086b4f2
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-04-20 14:52:39 +0530

    Optimized maxsum

--- SlidingWindow/MaxSumFrontLast.java
+++ SlidingWindow/MaxSumFrontLast.java
@@ -3,21 +3,17 @@ class MaxSumFrontLast {
         //your code goes here
         int i = 0;
         int n = cardScore.length;
-        int j = n - 1;
         int sum = 0;
         int temp = k;
         int leftSum = 0;
-        int rightSum = 0;
         while(temp != 0) {
             leftSum += cardScore[i++];
-            rightSum += cardScore[j--];
             temp--;
         }
-        sum = Math.max(leftSum, rightSum);
+        sum = leftSum;
         while(k != 0) {
             leftSum = leftSum - cardScore[k - 1] + cardScore[n - temp - 1];
-            rightSum = rightSum - cardScore[n - k] + cardScore[temp];
-            sum = Math.max(sum, Math.max(leftSum, rightSum));
+            sum = Math.max(sum, leftSum);
             temp++;
             k--;
         }
