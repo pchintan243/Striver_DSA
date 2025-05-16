570abb1a7fbc20e352d1565c39fdb679a81a276d
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-05-16 22:42:43 +0530

    remove unused func

--- Java_Recursion/GenerateParanthesis.java
+++ Java_Recursion/GenerateParanthesis.java
@@ -1,20 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 class GenerateParanthesis {
-    public boolean isValid(StringBuilder s, int n) {
-        int cnt = 0;
-        for(char ch : s.toString().toCharArray()) {
-            if(ch == '(') {
-                cnt++;
-            } else {
-                cnt--;
-            }
-            if(cnt < 0 || cnt > n) {
-                return false;
-            }
-        }
-        return cnt == 0;
-    }
     public void generateCombination(String s, int open, int close, int n, List<String> ans) {
         if(open + close == (n * 2)) {
             ans.add(s.toString());
