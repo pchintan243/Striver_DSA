ef678515bad44ad88759efb70908ce72973e075c
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-06-12 21:38:11 +0530

    12_3423_MaxDiff

---
+++ Daily_Problem/2025/06/12_3423_MaxDiff.java
@@ -0,0 +1,12 @@
+class 12_3423_MaxDiff {
+    public int maxAdjacentDistance(int[] nums) {
+        int n = nums.length;
+        int ans = Math.abs(nums[n - 1] - nums[0]);
+        
+        for(int i = 1; i < n; i++) {
+            ans = Math.max(ans, Math.abs(nums[i] - nums[i - 1]));
+        }
+
+        return ans;
+    }
+}
