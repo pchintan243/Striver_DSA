e93bd178fb9d88218b5d14f644bfe7f4a33f5dd8
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-06-29 15:27:28 +0530

    29_1498_NumberSubSequence

---
+++ Daily_Problem/2025/06/29_1498_NumberSubSequence.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+class 29_1498_NumberSubSequence {
+    public int numSubseq(int[] nums, int target) {
+        int mod = 1000000007 , n = nums.length;
+        Arrays.sort(nums);
+        
+        int[] power = new int[n];
+        power[0] = 1;
+        for (int i = 1; i < n; i++) {
+            power[i] = (power[i - 1] * 2) % mod;
+        }
+        int left = 0 , right = n - 1 , result = 0;
+
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                result = (result + power[right - left]) % mod;
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return result;
+    }
+}
