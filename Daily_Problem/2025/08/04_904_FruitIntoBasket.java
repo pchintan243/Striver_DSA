def089ff4a7b9b13a2cb7a4e8460e7d8c7ab657e
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-08-06 19:51:48 +0530

    04_904_FruitIntoBasket

---
+++ Daily_Problem/2025/08/04_904_FruitIntoBasket.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+class 04_904_FruitIntoBasket {
+    public int totalFruit(int[] fruits) {
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        int n = fruits.length;
+        int l = 0;
+        int r = 0;
+        int ans = 0;
+
+        while(r < n) {
+            mp.put(fruits[r], mp.getOrDefault(fruits[r], 0) + 1);
+            if(mp.size() > 2) {
+                if (mp.get(fruits[l]) == 1) {
+                    mp.remove(fruits[l]);
+                } else {
+                    mp.put(fruits[l], mp.get(fruits[l]) - 1);
+                }
+                l++;
+            }
+            
+            ans = Math.max(ans, r - l + 1);
+            r++;
+        }
+
+        return ans;
+    }
+}
