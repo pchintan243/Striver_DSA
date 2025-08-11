027bf4b6d702c07d21fda6bc2f253fee087af6f5
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-08-11 18:51:00 +0530

    PreOrderToBST

--- Binary Search Tree/Medium/PreOrderToBST.java
+++ Binary Search Tree/Medium/PreOrderToBST.java
@@ -1,3 +1,36 @@
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int data;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode(int val) { data = val; left = null, right = null }
+//  * }
+//  **/
+
+// class PreOrderToBST {
+//     private int ind = 0;
+//     TreeNode dfs(int[] preorder, int min, int max) {
+        
+//         if(ind == preorder.length || preorder[ind] <= min || preorder[ind] >= max) {
+//             return null;
+//         }
+//         TreeNode root = new TreeNode(preorder[ind]);
+//         ind++;
+
+//         root.left = dfs(preorder, min, root.data);
+//         root.right = dfs(preorder, root.data, max);
+//         return root;
+//     }
+//     public TreeNode bstFromPreorder(int[] preorder) {
+//         //your code goes here
+//         return dfs(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+//     }
+// }
+
+
+// only using max (Upper bound)
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -10,20 +43,20 @@
 
 class PreOrderToBST {
     private int ind = 0;
-    TreeNode dfs(int[] preorder, int min, int max) {
+    TreeNode dfs(int[] preorder, int max) {
         
-        if(ind == preorder.length || preorder[ind] <= min || preorder[ind] >= max) {
+        if(ind == preorder.length || preorder[ind] >= max) {
             return null;
         }
         TreeNode root = new TreeNode(preorder[ind]);
         ind++;
 
-        root.left = dfs(preorder, min, root.data);
-        root.right = dfs(preorder, root.data, max);
+        root.left = dfs(preorder, root.data);
+        root.right = dfs(preorder, max);
         return root;
     }
     public TreeNode bstFromPreorder(int[] preorder) {
         //your code goes here
-        return dfs(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        return dfs(preorder, Integer.MAX_VALUE);
     }
 }
