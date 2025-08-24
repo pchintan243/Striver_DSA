1b85aee25450c861ffc4a3c36c4becb2816ecaa0
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-08-24 12:52:28 +0530

    rm unused vars

--- Java_Graphs/Hard/DetectCycleDirectedGraph_DFS.java
+++ Java_Graphs/Hard/DetectCycleDirectedGraph_DFS.java
@@ -1,7 +1,6 @@
 package Java_Graphs.Hard;
-import java.util.LinkedList;
+
 import java.util.List;
-import java.util.Queue;
 
 public class DetectCycleDirectedGraph_DFS {
     boolean dfs(int v, List<List<Integer>> adj, boolean[] isVisited, boolean[] pathVisited) {
@@ -23,7 +22,6 @@ public class DetectCycleDirectedGraph_DFS {
         boolean[] isVisited = new boolean[N];
         boolean[] pathVisited = new boolean[N];
 
-        Queue<Integer> q = new LinkedList<>();
         for(int i = 0; i < N; i++) {
             if(!isVisited[i]) {
                 if(dfs(i, adj, isVisited, pathVisited)) {
