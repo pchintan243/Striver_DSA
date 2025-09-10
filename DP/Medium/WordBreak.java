213d1fd09fa74fc83fad422816480e9a87af0603
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-09-10 15:41:48 +0530

    WordBreak

---
+++ DP/Medium/WordBreak.java
@@ -0,0 +1,21 @@
+package Medium;
+import java.util.List;
+
+public class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for(int i = 1; i <= n; i++) {
+            for(String word: wordDict) {
+                int start = i - word.length();
+                if(start >= 0 && dp[start] && s.substring(start, i).equals(word)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
