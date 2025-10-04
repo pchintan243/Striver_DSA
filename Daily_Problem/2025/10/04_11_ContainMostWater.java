baff7bedceb916e8145a1c54ce30d6d52458dd14
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-10-04 13:42:14 +0530

    04_11_ContainMostWater

---
+++ Daily_Problem/2025/10/04_11_ContainMostWater.java
@@ -0,0 +1,20 @@
+package Daily_Problem.2025.10;
+
+class 04_11_ContainMostWater {
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int ans = 0;
+        while(l < r) {
+            if(height[l] < height[r]) {
+                ans = Math.max(ans, height[l] * (r - l));
+                l++;
+            } else {
+                ans = Math.max(ans, height[r] * (r - l));
+                r--;
+            }
+        }
+
+        return ans;
+    }
+}
