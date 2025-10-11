25913ddf0ef4f378a15e48efdd4b35d933dbf224
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-10-11 19:53:08 +0530

    10_3147_MaxEnergyDungeon

---
+++ Daily_Problem/2025/10/10_3147_MaxEnergyDungeon.java
@@ -0,0 +1,14 @@
+package Daily_Problem.2025.10;
+
+public class 10_3147_MaxEnergyDungeon {
+    public int maximumEnergy(int[] energy, int k) {
+        int l = energy.length;
+        int[] dp = new int[l];
+        int result = Integer.MIN_VALUE;
+        for (int i = l - 1; i >= 0; i--) {
+            dp[i] = energy[i] + (i + k < l ? dp[i + k] : 0);
+            result = Math.max(result, dp[i]);
+        }
+        return result;
+    }
+}
