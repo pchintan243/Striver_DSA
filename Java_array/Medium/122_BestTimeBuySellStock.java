02861196441e619f74656f1169184a879e7296cf
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-10-15 20:48:31 +0530

    122_BestTimeBuySellStock

---
+++ Java_array/Medium/122_BestTimeBuySellStock.java
@@ -0,0 +1,14 @@
+package Medium;
+
+public class 122_BestTimeBuySellStock {
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        int n = prices.length;
+        for(int i = 0; i < n - 1; i++) {
+            if(prices[i + 1] > prices[i]) {
+                ans += prices[i + 1] - prices[i];
+            }
+        }
+        return ans;
+    }
+}
