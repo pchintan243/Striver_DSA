945fb3937e8beab4bd0463cd3112276d38188c01
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-10-25 18:28:37 +0530

    62_UniquePaths

---
+++ DP/Grids/62_UniquePaths.java
@@ -0,0 +1,22 @@
+package Grids;
+import java.util.Arrays;
+
+public class 62_UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[] prev = new int[n];
+        Arrays.fill(prev, 1);
+
+        for(int i = 1; i < m; i++) {
+            int[] curr = new int[n];
+            curr[0] = 1;
+
+            for(int j = 1; j < n; j++) {
+                curr[j] = curr[j - 1] + prev[j];
+            }
+
+            prev = curr;
+        }
+
+        return prev[n - 1];
+    }
+}
