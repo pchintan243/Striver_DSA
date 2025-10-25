56dd9b63828685092b776bea7b37371eaab8a05c
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-10-25 13:43:47 +0530

    NinjaTraining

---
+++ DP/Medium/2D/NinjaTraining.java
@@ -0,0 +1,23 @@
+package Medium.2D;
+// Tabulation Form
+public class NinjaTraining {
+    public int ninjaTraining(int[][] matrix) {
+        int n = matrix.length;
+        int[][] dp = new int[n][3];
+
+        dp[0][0] = matrix[0][0];
+        dp[0][1] = matrix[0][1];
+        dp[0][2] = matrix[0][2];
+        for(int i = 1; i < n; i++) {
+            int temp = Math.max(dp[i - 1][1], dp[i - 1][2]);
+            dp[i][0] = temp + matrix[i][0];
+
+            temp = Math.max(dp[i - 1][0], dp[i - 1][2]);
+            dp[i][1] = temp + matrix[i][1];
+
+            temp = Math.max(dp[i - 1][0], dp[i - 1][1]);
+            dp[i][2] = temp + matrix[i][2];
+        }
+        return Math.max(dp[n - 1][0], Math.max(dp[n - 1][1], dp[n - 1][2]));
+    }
+}
