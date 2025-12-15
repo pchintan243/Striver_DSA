e0f87bb0a5e347bc3282c5eaaf4691a5a4be31ed
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-12-15 21:36:11 +0530

    10_3577_CntComputerUnblocking

---
+++ Daily_Problem/2025/12/10_3577_CntComputerUnblocking.java
@@ -0,0 +1,23 @@
+package Daily_Problem.2025.12;
+
+public class 10_3577_CntComputerUnblocking {
+    static final int MOD = 1_000_000_007;
+
+    public int countPermutations(int[] comp) {
+        int n = comp.length;
+        int first = comp[0];
+
+        // Check that first is the unique minimum
+        for (int i = 1; i < n; i++) {
+            if (comp[i] <= first) return 0;
+        }
+
+        // Compute factorial (n-1)!
+        long fact = 1;
+        for (int i = 2; i < n; i++) {
+            fact = (fact * i) % MOD;
+        }
+
+        return (int) fact;
+    }
+}
