0df8f1d07e52a57044712aa27737d43c674766d2
Chintan Patel <chintan.patel@artemhealthtech.com>
2025-12-27 13:55:44 +0530

    26_2483_MinimumPenaltyforShop

---
+++ Daily_Problem/2025/12/26_2483_MinimumPenaltyforShop.java
@@ -0,0 +1,21 @@
+package Daily_Problem.2025.12;
+
+public class 26_2483_MinimumPenaltyforShop {
+    public int bestClosingTime(String customers) {
+        int bestTime = 0;
+        int minPenalty = 0;
+        int prefix = 0;
+        
+        for (int i = 0; i < customers.length(); i++) {
+            char ch = customers.charAt(i);
+            prefix += ch == 'Y' ? -1 : 1;
+            
+            if (prefix < minPenalty) {
+                bestTime = i + 1;
+                minPenalty = prefix;
+            }
+        }
+        
+        return bestTime;
+    }
+}
