c64d7c52ee30f76fd0c438644677a9dcb26c5fc7
Chintan Patel <chintan.patel@artemhealthtech.com>
2026-01-10 12:41:53 +0530

    02_961_NRepeatedElementes

---
+++ Daily_Problem/2026/01/02_961_NRepeatedElementes.java
@@ -0,0 +1,18 @@
+package Daily_Problem.2026.01;
+import java.util.HashMap;
+import java.util.Map;
+
+public class 02_961_NRepeatedElementes {
+    public int repeatedNTimes(int[] A) {
+        Map<Integer, Integer> count = new HashMap();
+        for (int x: A) {
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+
+        for (int k: count.keySet())
+            if (count.get(k) > 1)
+                return k;
+
+        throw null;
+    }
+}
