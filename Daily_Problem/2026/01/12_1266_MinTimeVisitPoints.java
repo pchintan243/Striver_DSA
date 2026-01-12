bb1fcacca24c37926048082b037028d892509d00
Chintan Patel <chintan.patel@artemhealthtech.com>
2026-01-12 12:19:26 +0530

    12_1266_MinTimeVisitPoints

---
+++ Daily_Problem/2026/01/12_1266_MinTimeVisitPoints.java
@@ -0,0 +1,16 @@
+package Daily_Problem.2026.01;
+
+class 12_1266_MinTimeVisitPoints {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int ans = 0;
+        int n = points.length;
+        for(int i = 1; i < n; i++) {
+            ans += Math.max(
+                Math.abs(points[i][0] - points[i - 1][0]), 
+                Math.abs(points[i][1] - points[i - 1][1])
+            );
+        }
+
+        return ans;
+    }
+}
